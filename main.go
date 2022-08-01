fcbfbfdff3c2ad7d24fdd21baf1b5aeb8f87fe77
Nenad Paravinja <nenad.paravinja@fincore.com>
2022-08-01 21:12:15 +0200

    album added

--- main.go
+++ main.go
@@ -19,6 +19,7 @@ var albums = []album{
     {ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
     {ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
     {ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+    {ID: "4", Title: "Vojko", Artist: "Vojko V", Price: 19.99},
 }
 
 // getAlbums responds with the list of all albums as JSON.
